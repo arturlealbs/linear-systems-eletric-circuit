67a85c6e6ea190cd01178e251aa1f404e2bb3882
“arturlealbs” <albs@cin.ufpe.br>
2024-06-02 16:26:01 -0300

    Created Project

---
+++ Assets/Code/matrix_solver.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+// Não manjo de C#, ai embaixo tá as boas práticas de c# na godot
+//https://docs.godotengine.org/en/stable/tutorials/scripting/c_sharp/c_sharp_style_guide.html
+// Não recomendo usar c# pelo editor da Godot, melhor abrir o arquivo no VsCode
+/*
+Acredito que não tenha que se preocupar em aprender a usar a Godot, tudo vai ser resolvido no "backend"
+Só precisa botar as funções pra resolver dentro dessa classe
+*/ 
+
+public partial class MatrixSolver : Node
+{
+
+}
---
+++ Assets/Code/template.gd
@@ -0,0 +1,33 @@
+#tool
+#class_name
+#extends
+#documentation
+#signals
+#enums
+#constants
+#exported variables
+#public variables
+#private variables
+#onready variables
+#optional built-in virtual _init method
+#built-in virtual _ready method
+#remaining built-in virtual methods
+#public methods
+#private methods
+
+"""
+Naming Conventions
+Type	         Convention	     Info
+File names	     snake_case	     yaml_parsed.gd
+class_name	     PascalCase	     YAMLParser
+Node names	     PascalCase	     
+Functions	     snake_case	     
+Variables	     snake_case	     
+Signals	         snake_case	     always in past tense "door_opened"
+Constants	     CONSTANT_CASE	
+enum names	     PascalCase	
+enum members	 CONSTANT_CASE	
+"""
+# This is a comment. --> tem um espaço
+#print("This is disabled code") --> não dá espaço
+## This is a documentation --> duas hashtags com espaço
