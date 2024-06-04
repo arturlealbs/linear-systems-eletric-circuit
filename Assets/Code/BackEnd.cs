6e9bfc4fc849e2800e128bfae0e3539f46685905
“arturlealbs” <albs@cin.ufpe.br>
2024-06-04 00:10:21 -0300

    Created Backend

--- Assets/Code/BackEnd.cs
+++ Assets/Code/BackEnd.cs
@@ -3,11 +3,25 @@ using System;
 
 public partial class BackEnd : Node
 {
-	
-	// Called when the node enters the scene tree for the first time.
+	private MatrixData _matrixData;
+
 	public override void _Ready()
 	{
-		var matrixData = GetNode<MatrixData>("MatrixData");
-		matrixData.SetResistor(0, 1.5);
+		_matrixData = GetNode<MatrixData>("MatrixData");
+	}
+	public double[] HandleCircuitUpdateCall(int id, double r, double v)
+	{
+		_matrixData.SetComponents(id, r, v);
+
+		if (_matrixData.CheckForShortCircuit())
+		{
+			return null;
+		}
+		else
+		{
+			double [][] CoefficientsMatrix = _matrixData.GenerateCoefficientsMatrix();
+			double [] ConstantsMatrix = _matrixData.GenerateConstantsMatrix();
+			return MatrixSolver.SolveLinearSystem(CoefficientsMatrix, ConstantsMatrix);
+		}
 	}
 }
